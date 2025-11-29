12a231e3ee0fbba144ac89484915f19d15e66836
Timothy <timothyrwashburn@gmail.com>
2025-11-29 14:45:14 -0800

    create ios widget

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,17 @@
 import { Redirect } from 'expo-router';
 import { useUserDataStore } from "@/src/stores/useUserDataStore";
+import { ExtensionStorage } from "@bacons/apple-targets";
+import { useEffect } from "react";
+
+const widgetStorage = new ExtensionStorage('group.dev.timothyw.patapp')
 
 export default function Index() {
     const { getFirstModule } = useUserDataStore();
+
+    useEffect(() => {
+        widgetStorage.set('test', 'Hello World!');
+        ExtensionStorage.reloadWidget();
+    }, []);
 
     return <Redirect href={`/(tabs)/${getFirstModule()}`} />;
 }
